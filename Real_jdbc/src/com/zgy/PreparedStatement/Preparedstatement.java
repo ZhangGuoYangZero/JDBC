e98fdd0e6fd37fd5820a1e99a689bfee2baab54d
张国杨Zero <101501786+ZhangGuoYangZero@users.noreply.github.com>
2022-07-08 06:22:08 +0800

    1
    1

--- Real_jdbc/src/com/zgy/PreparedStatement/Preparedstatement.java
+++ Real_jdbc/src/com/zgy/PreparedStatement/Preparedstatement.java
@@ -1,17 +1,23 @@
 package com.zgy.PreparedStatement;
+import com.zgy.Mysqldriver;
 
-import com.zgy.mysqlDriver;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
-
 
 
 public class Preparedstatement {
 
     @Test
     public void test1() {
-    Connection conn = new mysqlDriver().getConn();
+        /**
+         * 方法注释
+        @param :
+        @return:void
+        @Description: 通用的关于增加 删除 修改的代码
+        */
+        Connection conn = Mysqldriver.getConnection();
+
     }
 
 }
--- Real_jdbc/src/com/zgy/mysqlDriver.java
+++ Real_jdbc/src/com/zgy/mysqlDriver.java
@@ -1,36 +1,84 @@
 package com.zgy;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.InputStream;
+import java.net.ConnectException;
+import java.sql.*;
 import java.util.Properties;
 
-public class mysqlDriver {
-    static  Properties  properties = new Properties();
-    String  url,user,password;
-    Connection conn;
-    static {
+/**
+ * @Description: nul
+ * @Date:2022/7/8 - 5:51
+ */
+public class Mysqldriver {
+    public static Connection getConnection() {
         try {
-            properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("conf.properties"));
-        }catch (IOException e){
-            System.out.println("not file");
-        }
-    }
-    public  mysqlDriver(){
-        try {
-            Class.forName(properties.getProperty("driverPath"));
-            this.url = properties.getProperty("url");
-            this.user = properties.getProperty("user");
-            this.password = properties.getProperty("password");
-            this.conn = DriverManager.getConnection(url,user,password);
-        }catch (ClassNotFoundException e){} catch (SQLException e) {
+            //造一个properties对象
+            Properties properties = new Properties();
+            //获取properties文件流,通过inputStream通过系统加载器
+            InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("conf.properties");
+            //获取properties文件的内容
+            properties.load(inputStream);
+            //获取4个元素，从properties对象中
+            String driverpath = properties.getProperty("driverPath");
+            String url = properties.getProperty("url");
+            String user = properties.getProperty("user");
+            String password = properties.getProperty("password");
+            //开始链接,先注册驱动
+            Class.forName(driverpath);
+            //通过驱动管理器获取链接
+            Connection conn = DriverManager.getConnection(url, user, password);
+            return conn;
+        } catch (IOException ioe) {
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        return null;
     }
 
-    public Connection  getConn(){
-        return  this.conn;
+
+    public static void closeAll(Connection conn, Statement statemt) {
+        /**
+         * 方法注释
+         @param conn: 链接对象
+         @param statemt: 信使对象
+         @return:boolean：确定是否正确
+         @Description:
+         */
+        try {
+            if (statemt != null)
+                statemt.close();
+            if (conn != null)
+                conn.close();
+        } catch (SQLException sql) {
+            System.out.println("close异常");
+        }
+
     }
 
+    public static void closeAll(Connection conn, Statement statemt, ResultSet resultSet) {
+        /**
+         * 方法注释
+         @param conn:
+         @param statemt:
+         @param resultSet:
+         @return:void
+         @Description:
+         */
+        try {
+            if (resultSet != null)
+                resultSet.close();
+            if (statemt != null)
+                statemt.close();
+            if (conn != null)
+                conn.close();
+        } catch (SQLException sql) {
+            System.out.println("close异常");
+        }
+
+    }
 }
